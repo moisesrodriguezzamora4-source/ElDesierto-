afd4ae4a6e6bef914d1f2c0c1b096c2f4b7edbf2
moisesrodriguezzamora4-source <moisesrodriguezzamora4@gmail.com>
2026-09-07 12:51:25 -0400

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://dcbtitlssuxdlutvifbm.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_g3iU-3HbnurvComNwdbeTg_QnxEpRir"
+
+const clienteSupabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
